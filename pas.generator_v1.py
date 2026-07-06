bd3b75aa5b27858e56767ad2b561a59842b99a35
Isa <isagilgaev@gmail.com>
2026-07-06 20:43:13 +0300

    add menu

---
+++ pas.generator_v1.py
@@ -0,0 +1,10 @@
+while True:
+    print("----------------------")
+    print("1 - Generate password")
+    print("2 - Exit")
+    print("----------------------")
+    choice = int(input("Choose: "))
+    if choice == 1:
+        print("Generating...")
+    elif choice == 2:
+        break
